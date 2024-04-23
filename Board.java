c076992f1894d6606b37fce7a3506574f8f3d4f7
daksh123456 <dakshkanaujia@gmail.com>
2024-04-24 00:25:35 +0530

    setting up the environment
    Signed-off-by: daksh123456 <dakshkanaujia@gmail.com>

---
+++ Board.java
@@ -0,0 +1,5 @@
+import java.util.*;
+public class Board {
+    int size;
+    List<List<Cell>> cellMatrix;
+}
---
+++ Game.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public class Game{
+    Board board;
+    List<Player> playerList;
+    Move nextMove;
+    List<Move> listOfMoves;
+    GameState gameState;
+}
